6360b9fa6e23ead616330cc5f144ca8345a2d79b
Gabriel Malheiro <gsmalheiro@rd.com.br>
2024-11-16 18:23:46 -0300

    feat: add project to github

---
+++ src/main/java/com/simple/springboot/project/springboot_simple_project/SpringbootSimpleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.simple.springboot.project.springboot_simple_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimpleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSimpleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simple/springboot/project/springboot_simple_project/SpringbootSimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.springboot.project.springboot_simple_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSimpleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
